36941a9e820e5d43d0b12d7fe648b26049a54c79
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-09-16 12:44:14 +0200

    first commit

---
+++ js1.js
@@ -0,0 +1,17 @@
+
+
+function wyswietl_wiek(){
+    let w = document.getElementById("wiek").value;
+
+    document.getElementById("wynik_wiek").innerHTML =  w ;
+
+
+}
+
+
+function wyswietl() {
+    let imie = document.getElementById("imie").value;
+    let nazwisko = document.getElementById("nazwisko").value;
+    
+    document.getElementById("wynik").innerHTML = "Imię: " + imie + ", Nazwisko: " + nazwisko;
+}
