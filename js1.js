48513d1168709937993e312c32e58d616c6e66d6
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2025-09-23 09:22:29 +0200

    m2

--- js1.js
+++ js1.js
@@ -9,6 +9,8 @@ function wyswietl_wiek(){
 
 }
 
+document.getElementById('wiek').addEventListener("change", wyswietl_wiek);
+
 
 function wyswietl() {
     let imie = document.getElementById("imie").value;
@@ -17,4 +19,33 @@ function wyswietl() {
     document.getElementById("wynik").innerHTML = "Imię: " + imie + ", Nazwisko: " + nazwisko;
 }
 
-document.getElementById('wiek').addEventListener("change", wyswietl_wiek);
+
+
+const wejscie = document.getElementById('wejscie1');
+    const wyjscie = document.getElementById('wyjscie1');
+
+    wejscie.addEventListener('input', function() {
+      const tekst = wejscie.value; // Pobranie wartości z pola
+      wyjscie.textContent = `Wprowadzono: ${tekst}`; // Wyświetlenie wprowadzonych danych
+    });
+
+
+
+
+    const form = document.getElementById('myForm');
+
+form.addEventListener('change', function(event) {
+  
+  event.preventDefault();
+
+  
+  const imie = document.getElementById('imief').value;
+  const nazwisko = document.getElementById('nazwiskof').value;
+  const wiek = document.getElementById('wiekf').value;
+
+  //console.log("Imię:", imie);
+  //console.log("Nazwisko:", nazwisko);
+  //console.log("Wiek:", wiek);
+    wyjscief.textContent = `Wprowadzono: ${imie} ${nazwisko} ${wiek}`;
+    
+});
